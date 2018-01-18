e01fa2304e99a80341ef20b26424254f0acf3a39
vignanlekkala <lekkalavignan@gmail.com>
2018-01-18 17:10:12 +0530

    Initial comment for the check-in

---
+++ Jenkins_Demo/run.bat
@@ -0,0 +1,6 @@
+set projectLocation=C:\Users\Nisum\Documents\Selenium_Practice\Jenkins_Demo
+cd C:\Users\Nisum\Documents\Selenium_Practice\Jenkins_Demo
+set classpath=C:\Users\Nisum\Documents\Selenium_Practice\Jenkins_Demo\bin;C:\Users\Nisum\Documents\Selenium_Practice\Jenkins_Demo\lib\*
+java org.testng.TestNG C:\Users\Nisum\Documents\Selenium_Practice\Jenkins_Demo\testng.xml
+pause
+
---
+++ Jenkins_Demo/src/JenkinsDemo.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JenkinsDemo {
+    
+	WebDriver driver;
+	@Test
+	public void open()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nisum\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.macys.com/account/profile");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		
+		System.out.println("Macys application opened ");
+		
+		List<WebElement> elements=driver.findElements(By.xpath("//*[@id='month']//option"));
+		
+		
+		
+		for(WebElement element:elements)
+		{
+			String text=element.getText();
+			
+			if(text.equals("April"))
+		 {
+			element.click();
+			break;
+		 }
+				System.out.println("Hello");
+				
+			}
+	}
+}
